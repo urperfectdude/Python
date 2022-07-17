49bd4ae2a27020bd0588d511c4cf820fe767828e
Prashant Nayak <41485907+urperfectdude@users.noreply.github.com>
2022-07-18 00:45:49 +0530

    Update main.py

--- Blind_Auction/main.py
+++ Blind_Auction/main.py
@@ -3,27 +3,30 @@ from art import logo
 print(logo)
 
 bids = {}
-bidding_finished = False
+bid_finish = False
 
-def find_highest_bidder(bidding_record):
-  highest_bid = 0
-  winner = ""
-  # bidding_record = {"Angela": 123, "James": 321}
-  for bidder in bidding_record:
-    bid_amount = bidding_record[bidder]
-    if bid_amount > highest_bid: 
-      highest_bid = bid_amount
-      winner = bidder
-  print(f"The winner is {winner} with a bid of ${highest_bid}")
 
-while not bidding_finished:
-  name = input("What is your name?: ")
-  price = int(input("What is your bid?: $"))
-  bids[name] = price
-  should_continue = input("Are there any other bidders? Type 'yes or 'no'.\n")
-  if should_continue == "no":
-    bidding_finished = True
-    find_highest_bidder(bids)
-  elif should_continue == "yes":
-    clear()
-  
+def highest_bid (bid_record):
+    most_highest_bid = 0
+    winner = ""
+    for bids in bid_record:
+        bid_amount = bid_record[bids]
+        if bid_amount > most_highest_bid:
+            most_highest_bid = bid_amount
+            winner = bids
+
+    print (f"The winner is {name} with amount of Rs.{price}")
+
+
+while not bid_finish:
+    name = input("whats ur name?")
+    price = int(input("whats ur bid? Rs."))
+    bids[name]=price
+    go_ahead= input("Any more bidders? 'y/n'")
+        
+    if go_ahead == 'n':
+        bid_finish=True
+        highest_bid(bids)
+    elif go_ahead == 'n':
+        clear()
+        
