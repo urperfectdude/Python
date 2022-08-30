a6b32dfb5300f96a307366feab01a5261973b30d
Prashant Nayak <41485907+urperfectdude@users.noreply.github.com>
2022-08-30 09:10:11 +0530

    Update Snake_game.py

--- Turtle/Snake_game.py
+++ Turtle/Snake_game.py
@@ -7,17 +7,6 @@ screen.title("My snake Game")
 
 screen.exitonclick()
 
-segment_1= Turtle("square")
-segment_1.color("white")
-
-segment_2= Turtle("square")
-segment_2.color("white")
-segment_2.goto(-20,0)
-
-segment_3= Turtle("square")
-segment_3.color("white")
-segment_3.goto(-40,0)
-
 starting_positions=[(0,0),(-20,0),(-40,0)]
 segments = []
 
